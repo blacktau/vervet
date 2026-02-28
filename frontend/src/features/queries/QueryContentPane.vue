51d89f03fdf3f11c32027c2536991babfe551bd1
Sean Garrett <sean@blacktau.com>
2026-02-28 18:32:08 +0000

    added subtabs

--- frontend/src/features/queries/QueryContentPane.vue
+++ frontend/src/features/queries/QueryContentPane.vue
@@ -1,9 +1,33 @@
 <script lang="ts" setup>
+import { BrowserSubTabType } from '@/consts/BrowserSubTabType'
+import { useTabStore } from '@/features/tabs/tabs'
+import { useI18n } from 'vue-i18n'
+import QueryTab from './QueryTab.vue'
+import { CodeBracketSquareIcon } from '@heroicons/vue/24/outline'
+
+const tabStore = useTabStore()
+const { t } = useI18n()
 </script>
 
 <template>
   <div class="content-container flex-box-v">
-    queries go here
+    <n-tabs
+      :value="tabStore.currentSubTab"
+      type="line"
+      animated
+      @update:value="tabStore.switchSubTab"
+    >
+      <n-tab-pane
+        :name="BrowserSubTabType.Query"
+        display-directive="show:lazy"
+      >
+        <template #tab>
+          <n-icon :component="CodeBracketSquareIcon" style="margin-right: 4px" />
+          {{ t('dataBrowser.subTab.query') }}
+        </template>
+        <QueryTab />
+      </n-tab-pane>
+    </n-tabs>
   </div>
 </template>
 
